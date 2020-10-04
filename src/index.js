9dd5c60512063c31e46d69a68d4caf1ccf28ef66
emarf <frame.toravich@gmail.com>
2020-10-04 14:59:03 +0300

    feat: implement human-readable-number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+
+    const numbersLessNineteen = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const dozen = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const hundred = ['hundred'];
+
+    let numStr = number.toString();
+
+    if (number > 99) {
+        if (number % 100 === 0) {
+            return `${numbersLessNineteen[numStr.charAt(0)]} ${hundred}`;
+        } else {
+            let reminderOfDivision = number % 100;
+            let temp
+            if (reminderOfDivision <= 19) {
+                temp = numbersLessNineteen[reminderOfDivision];
+            } else {
+                temp = (reminderOfDivision % 10 === 0) ? dozen[numStr.charAt(1)] : `${dozen[numStr.charAt(1)]} ${numbersLessNineteen[numStr.charAt(2)]}`;
+            }
+            return `${numbersLessNineteen[numStr.charAt(0)]} ${hundred} ${temp}`;
+        }
+    } else if (number > 19 && number < 100) {
+        return (number % 10 === 0) ? dozen[numStr.charAt(0)] : `${dozen[numStr.charAt(0)]} ${numbersLessNineteen[numStr.charAt(1)]}`;
+    } else {
+        return numbersLessNineteen[number];
+    }
 }
